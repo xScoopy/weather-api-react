148daa57e500f2ea84ada571c241234b0ac8d583
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-04-04 02:20:35 -0500

    added select and radio buttons for unit choice

---
+++ src/RadioButton.js
@@ -0,0 +1,15 @@
+const RadioButton = (props) => {
+    const { label, name, checked, onChange } = props
+  return (
+    <label>
+      <input
+        type="radio"
+        name={name}
+        checked={checked}
+        onChange={onChange}
+      />
+      {label}
+    </label>
+  );
+};
+export default RadioButton;
--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,55 @@
 import { useState } from "react";
+import RadioButton from "./RadioButton";
 import "./Weather.css";
 const Weather = () => {
   const [zip, setZip] = useState("55076");
+  const [unit, setUnit] = useState("imperial");
 
   return (
     <div className="Weather">
+      <h1>
+        {zip} {unit}
+      </h1>
       <form>
-        <input 
+        <div>
+          <input
             placeholder="Enter Zip Code"
             value={zip}
-            onChange={(e) => {setZip(e.target.value)}}
+            onChange={(e) => {
+              setZip(e.target.value);
+            }}
+          />
+          <button>Submit</button>
+        </div>
+        <select
+          onChange={(e) => {
+            setUnit(e.target.value);
+          }}
+          value={unit}
+        >
+          <option value="metric">Celsius</option>
+          <option value="imperial">Fahrenheit</option>
+          <option value="standard">Kelvin</option>
+        </select>
+
+        <RadioButton
+          name="unit"
+          label="metric"
+          checked={unit === "metric"}
+          onChange={() => setUnit("metric")}
         />
-        <button>Submit</button>
+        <RadioButton
+          name="unit"
+          label="imperial"
+          checked={unit === "imperial"}
+          onChange={() => setUnit("imperial")}
+        />
+        <RadioButton
+          name="unit"
+          label="standard"
+          checked={unit === "standard"}
+          onChange={() => setUnit("standard")}
+        />
       </form>
     </div>
   );
